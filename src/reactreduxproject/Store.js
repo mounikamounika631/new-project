595e2a9b3b276bd52266b464bfa24df058ac1ae9
mounikamounika631 <124336669+mounikamounika631@users.noreply.github.com>
2023-03-31 16:49:49 +0530

    Delete src/reactreduxproject directory

--- src/reactreduxproject/Store.js
+++
@@ -1,9 +0,0 @@
-import {createStore,applyMiddleware} from 'redux';
-import {composeWithDevTools} from "redux-devtools-extension";
-import thunk from "redux-thunk";
-
-const middleware=[thunk]
-const store=createStore(reducer,
-    composeWithDevTools(applyMiddleware(...middleware)))
-
-    export default store;
